086238c7b19ee2c86b69f5fc0fe0b4792db5c8bf
Nwsheats <nwsheats@gmail.com>
2023-01-30 20:25:33 -0600

    fixed routing for portfolioContainer

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,91 +1,63 @@
-import React from 'react';
-import '../styles/Nav.css';
-import '../styles/Header.css';
-import image from '../images/abstract.png';
+import React from "react";
+import "../styles/Nav.css";
 
 const styles = {
   navStyle: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '60%',
-    height: '100px',
-    padding: '2%',
-    listStyleType: 'none'
+    display: "flex",
+    justifyContent: "space-between",
+    width: "60%",
+    height: "100px",
+    padding: "2%",
+    listStyleType: "none",
   },
   navFont: {
-    fontSize: '40px',
-  }
-};
-
-const stylesHeader = {
-  divBg: {
-      display: 'flex',
-      flexDirection: 'row',
-      backgroundImage: `url(${image})`,
-      backgroundPosition: 'center',
-      backgroundSize: '2000px',
-      height: '200px',
-      width: '100%',
-  },
-  headerBg: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "flex-start",
-      padding: "2%",
-      width: "40%",
-      color: "#fff",
-    },
-      headerFont: {
-      fontSize: '50px',
-      fontFamily: "Arial, Helvetica, sans-serif",
-      alignSelf: "flex-start",
+    fontSize: "40px",
   },
 };
 
-
-export function Header() {
+function Nav({ currentPage, handlePageChange }) {
   return (
-      <div style={stylesHeader.divBg} className='divBg'>
-      <header style={stylesHeader.headerBg} className="header">
-          <h3 style={stylesHeader.headerFont}>Nathan Sheats</h3>
-      </header>
-      </div>
-  )
+    <ul style={styles.navStyle} className="nav nav-tabs">
+      <li style={styles.navFont} className="nav-item">
+        <a
+          href="#aboutMe"
+          onClick={() => handlePageChange("AboutMe")}
+          className={currentPage === "AboutMe" ? "nav-link active" : "nav-link"}
+        >
+          About Me
+        </a>
+      </li>
+      <li style={styles.navFont} className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange("Portfolio")}
+          className={
+            currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+          }
+        >
+          Portfolio
+        </a>
+      </li>
+      <li style={styles.navFont} className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+      <li style={styles.navFont} className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange("Resume")}
+          className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
 }
 
-  
-export function Nav({ currentPage, handlePageChange }) {
-    return (
-      <ul style={styles.navStyle} className="nav nav-tabs">
-        <li style={styles.navFont} className="nav-item">
-          <a href="#AboutMe" onClick={() => handlePageChange('AboutMe')} 
-          className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}>
-            About Me
-          </a>
-        </li>
-        <li style={styles.navFont} className="nav-item">
-          <a href="#Portfolio" onClick={() => handlePageChange('Portfolio')}
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
-            Portfolio
-          </a>
-        </li>
-        <li style={styles.navFont} className="nav-item">
-          <a href="#Contact"
-  
-            onClick={() => handlePageChange('Contact')}
-            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-          >
-            Contact
-          </a>
-        </li>
-        <li style={styles.navFont} className="nav-item">
-          <a href="#resume"
-  
-            onClick={() => handlePageChange('Resume')}
-            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
-            Resume
-          </a>
-        </li>
-      </ul>
-    );
-  }
+export default Nav;
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,33 +1,10 @@
-import React, { useState } from 'react';
-import {Nav, Header} from './Nav';
-import AboutMe from './AboutMe';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
-import Resume from './Resume';
+import React from "react";
+import Header from "./Header";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('AboutMe');
-
-  const renderPage = () => {
-    if (currentPage === 'AboutMe') {
-      return <AboutMe />;
-    }
-    if (currentPage === 'Portfolio') {
-      return <Portfolio />;
-    }
-    if (currentPage === 'Contact') {
-      return <Contact />;
-    }
-    return <Resume />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
   return (
     <div>
       <Header />
-      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
-      {renderPage()}
     </div>
   );
 }
