764f5f50cbec34a9037f6828a19a4cd93741c300
Nwsheats <nwsheats@gmail.com>
2023-01-31 11:12:52 -0600

    added formatting to Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,60 +1,89 @@
 import React from 'react';
 import '../styles/Projects.css';
-import image from '../images/triangles.jpg';
-import image2 from '../images/collage.jpg';
+import image from '../images/gears.jpg';
+import image2 from '../images/bricks.jpg';
+import image3 from '../images/ocean.jpg';
+import image4 from '../images/sunset.jpg';
+import image5 from '../images/circles.jpg';
+import image6 from '../images/colors.jpg';
 
 
-const projectLink = []
+const projectNames = [
+    {
+    name: "",
+    imgLink: image,
+    imgURL: ""
+    },
+    {
+    name: "",
+    imgLink: image2,
+    imgURL: ""
+    },
+    {
+    name: "",
+    imgLink: image3,
+    imgURL: ""
+    },
+    {
+    name: "",
+    imgLink: image4,
+    imgURL: ""
+    },
+    {
+    name: "",
+    imgLink: image5,
+    imgURL: ""
+    },
+    {
+    name: "",
+    imgLink: image6,
+    imgURL: ""
+    },
+]
 
 const styles = {
     divStyle: {
         display: "flex",
+        justifyContent: "center",
+        flexDirection: 'row',
+        border: '1px solid',
+        width: '100%',
+        flexWrap: 'wrap',
+        background: '#5CC8FF'
     },
     projectStyle: {
         display: 'flex',
-        border: '1px solid',
-        height: 'auto',
-        width: '80%',
-        backgroundImage: `url(${image})`
-    },
-    projectImage: {
-        width: '80%',
-        height: '1000px',
+        flexDirection: 'column',
         border: '5px solid #817fff',
-        backgroundPosition: 'center',
+        margin: '75px',
+        height: '300px',
+        width: '25%',
         backgroundSize: 'contain'
     },
     projectFont: {
-        fontSize: '20px'
+        fontSize: '20px',
+        border: '1px solid',
     },
     secondDivStyle: {
-        background: '',
-        border: '1px solid'
+        border: '1px solid',
+        height: '300px',
+        width: '100%',
     }
 }
 
-const projectNames = [
-    {
-    name: "project1",
-    imgLink: image
-    },
-    {
-    name: "project2",
-    imgLink: image2
-    }
-]
 
 
 
 function Projects() {
     return (
-        <div>
-        {projectNames.map((project) =>        
-        <div style={{backgroundImage: `url(${project.imgLink})`}} className="projectDiv">
-        <img style={styles.projectImage} src={project.imgLink} alt=''></img>
-        <div style={styles.secondDivStyle} className="secondDiv">
-        <h3 style={styles.projectFont} className="pFont">{project.name}</h3>
-        <h5>Project</h5>
+        <div style={styles.divStyle}>
+        {projectNames.map((project) =>
+        <div style={styles.projectStyle}>
+        <div style={styles.secondDivStyle}>
+        <div><a href={project.imgURL}><img style={{backgroundImage: `url(${project.imgLink})`}} 
+        className="projectDiv" src={project.imgLink} alt='' width='100%' height='300px'>
+        </img></a>
+        </div>
         </div>
         </div>)}
         </div>
